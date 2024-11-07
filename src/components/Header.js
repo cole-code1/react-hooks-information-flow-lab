0739b3f013941f9826d7e0844087b16c6e464d54
Collins Kathurima <collinskathu313@gmail.com>
2024-11-07 16:48:14 +0300

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Header({ isDarkMode, onDarkModeClick}) {
+    return(
+        <header>
+            <h2>Shopster</h2>
+            <button onClick={onDarkModeClick}>
+                {isDarkMode ? "Dark" : "Light"} Mode
+            </button>
+        </header>
+    );
+    
+}
